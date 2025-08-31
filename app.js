4438aac9626fc9380c2e9e08456381f377f44856
avinish321 <avinish729@gmail.com>
2025-09-01 03:46:15 +0530

    change made

--- app.js
+++ app.js
@@ -1,125 +1,118 @@
-if(process.env.NODE_ENV !== "production") {
-     require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
 }
-const Razorpay = require('razorpay');
+
 const express = require("express");
-const app=express();
-const path=require("path");
-const mongoose=require("mongoose");
-const methodOverride=require("method-override");
-const ejsMate= require("ejs-mate");
-const ExpressError=require("./utils/ExpressError.js");
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
-const LocalStrategy = require ("passport-local");
+const LocalStrategy = require("passport-local");
+const Razorpay = require("razorpay");
+
+// Models & Schemas
 const User = require("./models/user.js");
-const {listingSchema,reviewSchema} =require("./schema.js");
-const Review =require("./models/reviews.js");
-const { options } = require("joi");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/reviews.js");
+
+// Routes
+const listingsRoutes = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 const paymentRouter = require("./routes/paymentRoutes.js");
 
-
-
-
-const listingsRoutes = require("./routes/listing.js");
-const reviewRouter = require ("./routes/review.js");
-const userRouter =require ("./routes/user.js");
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/Wanderlust";
-
+// DB Connection
 const dburl = process.env.ATLASDB_URL;
-main()
-.then(() =>{
-    console.log("connected to db");
-})
-.catch((error)=>{
-    console.log(error);
-});
- async function main(){
-      await mongoose.connect(dburl);
- }
- app.engine('ejs', ejsMate);
- app.set("view engine","ejs");
- app.set("views", path.join(__dirname, "views"));
- app.use(express.urlencoded({extended:true}));
- // Parse JSON body
+
+mongoose.connect(dburl)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log("MongoDB connection error:", err));
+
+// Initialize Express
+const app = express();
+app.engine('ejs', ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
- app.use(methodOverride("_method"));
- app.use(express.static(path.join(__dirname, "public")));
- app.use('/uploads', express.static('uploads'));
- 
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use('/uploads', express.static('uploads'));
 
+// Razorpay
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET
-});
-const store = MongoStore.create({
-    mongoUrl :dburl,
-    crypto :{
-        secret:"mysupersecretcode",
-    },
-    touchAfter: 24 * 3600,
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
- store.on("error", () =>{
-     console.log("ERROR in MONGO SESSION STORE" ,err);
- });
+// Session Store
+const store = MongoStore.create({
+    mongoUrl: dburl,
+    crypto: { secret: "mysupersecretcode" },
+    touchAfter: 24 * 3600, // 1 day
+});
+
+store.on("error", (err) => {
+    console.log("ERROR in MONGO SESSION STORE:", err);
+});
+
+// Session Config
 const sessionOptions = {
     store,
-    secret:"mysupersecretcode",
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        expires :Date.now() + 7 * 24 *60 * 60 *1000 ,
-        maxAge: 7 * 24 * 60 * 60 *1000,
-        httpOnly:true,
-     secure: process.env.NODE_ENV === "production",
-    },
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production"
+    }
 };
 
-
-
- 
-// app.get("/", (req, res) =>{
-//          res.send("/avinish....");
-// });
-
-  
-app.use( session ( sessionOptions));
+app.use(session(sessionOptions));
 app.use(flash());
 
+// Passport Config
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next) =>{
+// Flash & Current User Middleware
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
     next();
 });
 
+// Routes
 app.use("/listings", listingsRoutes);
-app.use("/listings/:id/reviews" , reviewRouter);
+app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 app.use("/payment", paymentRouter);
 
- 
-// app.all("*", (req, res, next) => {
-//   next(new ExpressError("Page Not Found", 404));
-// });
+// 404 Handler
+app.all("*", (req, res, next) => {
+    res.status(404).render("error.ejs", { message: "Page Not Found" });
+});
 
- app.use((err, req, res, next) =>{
-     let {statusCode=500, message ="something went wrong"}= err;
-     res.status(statusCode).render("error.ejs", {message});
- });
-``
+// Error Handler
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).render("error.ejs", { message });
+});
 
-
-app.listen(8080, ()=>{
-    console.log("server is listening to post 8080");
+// Start Server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
