0c242d73b703d39e1d88aadbd7b9ea1747830292
Avinish Kumar <avi77sub@gmail.com>
2025-09-01 21:11:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
